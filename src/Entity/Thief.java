c0dac0ecc265793ba16259e93fce0ac6803e00ff
Tiago Oliveira <tiago9@ua.pt>
2017-03-17 16:18:34 +0000

    Initial

---
+++ src/Entity/Thief.java
@@ -0,0 +1,9 @@
+package Entity;
+
+/**
+ *
+ * @author Tiago Oliveira tiago9@ua.pt n.:51687
+ */
+public class Thief {
+    
+}
---
+++ src/HeistMuseum/HeistMuseum.java
@@ -0,0 +1,16 @@
+package HeistMuseum;
+
+/**
+ *
+ * @author Tiago Oliveira tiago9@ua.pt n.:51687
+ */
+public class HeistMuseum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
