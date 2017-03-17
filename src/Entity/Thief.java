7da816a82f3071a6f6075842f29b2f9f2cfe89bc
Tiago Oliveira <tiago9@ua.pt>
2017-03-17 17:19:51 +0000

    Commit just to level for gito

--- src/Entity/Thief.java
+++ src/Entity/Thief.java
@@ -2,10 +2,66 @@ package Entity;
 
 // import das areas with which the thief will interact
 /**
+ * Explanation of what this class does or provides
  *
  * @author Tiago Oliveira tiago9@ua.pt n.:51687
  */
+public class Thief extends Thread {
 
-public class Thief {
+    /**
+     * Identification of the Thief
+     *
+     * @serialField thiefId
+     */
+    private int thiefId;
+
+    /**
+     * State of the Thief
+     *
+     * @serialField stateThief
+     */
     
+    private int stateThief;
+
+    /**
+     * State of the Thief
+     *
+     * @serialField agility
+     */
+    
+    private int agility;
+    private int position; // not yet sure, here or assault party
+    
+    
+  /**
+   *  Thief thread instantiation
+   *
+   *    @param thiefId Thief identification 
+   *    @param agility Thief agility 
+   *    @param nIter número de iterações do ciclo de vida do cliente
+   */
+
+   public Thief (int thiefId, int agility){
+   
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 }
---
+++ src/World/GeneralRepositoryInformation.java
@@ -0,0 +1,9 @@
+package World;
+
+/**
+ *
+ * @author Tiago Oliveira tiago9@ua.pt n.:51687
+ */
+public class GeneralRepositoryInformation {
+    
+}
