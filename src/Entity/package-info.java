e1c5bbcd63c3e6fdde25be0037273aba08207b93
Tiago Oliveira <tiago9@ua.pt>
2017-03-26 23:37:16 +0100

    Revert "antes da documentacao"
    This reverts commit 18912e2f5644538930690efbe6bbb62c5f7682c8.

--- src/Entity/package-info.java
+++ src/Entity/package-info.java
@@ -1,8 +1,3 @@
-/**
-* Explicit monitors implementation of Heist the museum.
-* This package contains the active entities (master thief and ordinary thieves).
-*
-* @author Tiago Oliveira, tiago9@ua.pt, no.: 51687
-* @author João Cravo, joao.cravo@ua.pt, no.: 63784
-*/
 package Entity;
+
+// test Bitbucket
--- src/HeistMuseum/package-info.java
+++ src/HeistMuseum/package-info.java
@@ -1,9 +1,1 @@
-/**
-* Explicit monitors implementation of Heist the museum.
-* This package contains the main program that runs the problem. 
-* Also contains the constants.
-*
-* @author Tiago Oliveira, tiago9@ua.pt, no.: 51687
-* @author João Cravo, joao.cravo@ua.pt, no.: 63784
-*/
 package HeistMuseum;
